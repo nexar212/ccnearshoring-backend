7a9680fb1828b738694b7223229e421b0de6d567
Alexis Félix <alexis.felix@coppel.com>
2024-09-02 23:40:25 -0700

    Token via session storage

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,35 +2,29 @@ const User = require('../models/users');
 const bcrypt = require('bcryptjs');
 const  jwt = require('jsonwebtoken');
 const  {verify} = require('jsonwebtoken');
-const { serialize } = require('cookie')
 
 exports.profile = async (req, res) => {
     try {
-        const {myToken} = req.cookies;
-        const userData = verify(myToken, process.env.JWT_SECRET);
-        const userEmail = userData.user.email
+
+        const token = req.headers.authorization?.split(' ')[1];
+
+        const userData = verify(token, process.env.JWT_SECRET);
+
+        const userEmail = userData.email
 
         const user = await User.findOne({ email: userEmail});
 
         if (!user) {
-            return res.status(40).json({ msg: 'User not found' });
+            return res.status(400).json({ msg: 'User not found' });
         }
 
-        res.json({
+        res.status(200).json({
             firstName: user.firstName,
             lastName: user.lastName,
             email: user.email,
             role: user.role,
         })
-        res.status(200)
     } catch (error) {
-        res.cookie('myToken', '', {
-            httpOnly: false,
-            secure: false,
-            sameSite: 'lax',
-            expires: new Date(0),
-            path: '/'
-        });
         res.status(401).json({ error: 'invalid token'})
     }
 };
@@ -51,7 +45,7 @@ exports.register = async (req, res) => {
             role
         });
 
-        const response = await user.save();
+        await user.save();
 
         res.status(201).json({ msg: 'User registered successfully' });
     } catch (err) {
@@ -60,10 +54,11 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-    const { email, password } = req.body;
 
-    try {
+    try{
+        const { email, password } = req.body;
         const user = await User.findOne({ email });
+        
         if (!user) {
             return res.status(400).json({ msg: 'Invalid credentials' });
         }
@@ -77,48 +72,15 @@ exports.login = async (req, res) => {
             return res.status(400).json({ msg: 'Invalid credentials' });
         }
 
-        const payload = {
-            user: {
-                id: user.id,
-                email: user.email,
-                name: user.firstName,
-                role: user.role
-            }
-        };
-
-        const token = jwt.sign(
-            payload,
-            process.env.JWT_SECRET,
-            { expiresIn: 60 }
-        );
-
-        const serialized = serialize('myToken', token, {
-            httpOnly: false,
-            secure: false,
-            sameSite: 'lax',
-            maxAge: 1000 * 60,
-            path: '/'
-        });
-
-        res.setHeader('Set-Cookie', serialized)
-        res.status(200).json({message: 'log in succesfully'})
+        const token = jwt.sign({ 
+            id: user.id,
+            email: user.email,
+            name: user.firstName,
+            role: user.role
+         }, process.env.JWT_SECRET, { expiresIn: '1h' });
+ 
+        res.status(200).json({message: 'log in succesfully', token})
     } catch (err) {
         res.status(500).send('Server error');
     }
 };
-
-exports.logout = async (req, res) => {
-    try {
-        res.cookie('myToken', '', {
-            httpOnly: false,
-            secure: false,
-            sameSite: 'lax',
-            expires: new Date(0),
-            path: '/'
-        });
-    
-        res.status(200).json({ message: 'Logout successful' });
-    } catch (error) {
-        return res.status(401).json({error: 'Invalid token'})
-    }
-}
--- server.js
+++ server.js
@@ -18,6 +18,3 @@ app.use('/api/auth', authRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-console.log('============== Prueba variables de entorno =============')
-console.log(process.env.MONGO_URL, process.env.FRONT_END_URL)
-console.log('======================================================')
